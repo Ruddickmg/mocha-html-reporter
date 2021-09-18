0e2b299ff612c97c18a8a13268910393c72191c6
ruddickmg <ruddickmg@gmail.com>
2021-09-18 07:19:34 -1000

    remove unused file path formatter

--- src/formatting/paths.ts
+++ src/formatting/paths.ts
@@ -6,11 +6,6 @@ export const formatOutputPath = (
   outputDir: string,
 ): string => `${resolve(process.cwd(), outputDir)}${PATH_SEPARATOR}`;
 
-export const formatOutputFilePath = (
-  outputDir: string,
-  fileName: string,
-): string => `${formatOutputPath(outputDir)}${fileName}.html`;
-
 export const removeFileName = (pathToFile: string): string => {
   const fileName = pathToFile.split(PATH_SEPARATOR).pop();
   const toRemove = `${PATH_SEPARATOR}${fileName}`;
